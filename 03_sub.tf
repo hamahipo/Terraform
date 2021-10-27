994b4ff68fe9bd0bb4d2add418517b7edf1ea856
hamahipo <86971120+hamahipo@users.noreply.github.com>
2021-10-27 23:35:24 +0900

    subnet_step3

--- 03_sub.tf
+++ 03_sub.tf
@@ -20,7 +20,7 @@ resource "aws_subnet" "yljo_pria" {
   }
 }
 
-# 가용영역 a의 Public Subnet
+# 가용영역 c의 Public Subnet
 resource "aws_subnet" "yljo_pubc" {
   vpc_id            = aws_vpc.yljo_vpc.id
   cidr_block        = "10.0.2.0/24"
@@ -31,7 +31,7 @@ resource "aws_subnet" "yljo_pubc" {
   }
 }
 
-# 가용영역 a의 Private Subnet
+# 가용영역 c의 Private Subnet
 resource "aws_subnet" "yljo_pric" {
   vpc_id            = aws_vpc.yljo_vpc.id
   cidr_block        = "10.0.3.0/24"
