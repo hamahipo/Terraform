23872f11b8073eb643a47b8da7d347da86db145a
yljo <noppy1004@gmail.com>
2021-10-30 10:59:55 +0900

    atatt_step19

---
+++ 19_atatt.tf
@@ -0,0 +1,5 @@
+resource "aws_autoscaling_attachment" "yljo-atatt" {
+    autoscaling_group_name = aws_autoscaling_group.yljo_atsg.id
+    alb_target_group_arn = aws_lb_target_group.yljo_lbtg.arn
+    
+}
